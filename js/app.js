71eee8f742b3b0bed7663792a85a866fd31d9e65
Connor Freeman <connormfreeman@gmail.com>
2015-04-01 15:56:02 -0500

    bounds checking on player

--- js/app.js
+++ js/app.js
@@ -8,7 +8,11 @@ var Enemy = function(pos) {
     this.sprite = 'images/enemy-bug.png';
     this.x = pos.x;
     this.y = pos.y;
-    this.speed = 10;
+    this.speed = enemySpeed(3, 7); // between 3 and 7
+
+    function enemySpeed(min, max) {
+        return Math.floor(Math.random() * max) + max;
+    }
 
     return this;
 }
@@ -21,10 +25,10 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     if (this.x > 550) {
-        this.x = -100;
+        this.x = -101;
     } else {
         this.x = this.x + this.speed;
-        console.log(Resources.get(this.sprite).width);
+        // console.log(Resources.get(this.sprite).width);
     }
 
     ctx.drawImage(Resources.get(this.sprite), this.x*dt, this.y*dt);
@@ -44,12 +48,16 @@ var Player = function(pos) {
     this.x = pos.x;
     this.y = pos.y;
 
+    this.MOVEX = 101;
+    this.MOVEY = 83;
+
     return this;
 }
 
 
 Player.prototype.render = function() {
-    // console.log(Player.position);
+    // console.log(this.x);
+
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
@@ -63,27 +71,55 @@ Player.prototype.update = function() {
 }
 
 Player.prototype.handleInput = function(userInput) {
+    // TODO:
+    // boolean isValidMove()
+    // return true if in bounds, or if no errors
 
     // Move player 
-    switch(userInput) {
-        case 'left':
-            this.x = this.x - 101;
-        break;
-        case 'right':
-            this.x = this.x + 101;
-        break;
-        case 'up':
-            this.y = this.y - 85;
-        break;
-        case 'down':
-            this.y = this.y + 85;
-        break;
-    }
+        switch(userInput) {
+            case 'left':
+                var nextXmove = this.x - this.MOVEX;
+                if(nextXmove >= -2) {
+                    this.x = this.x - this.MOVEX;
+                } else {
+                    this.resetPlayer();
+                }
+            break;
+            case 'right':
+                var nextXmove = this.x + this.MOVEX;
+                if(nextXmove <= 500) {
+                    this.x = this.x + this.MOVEX;
+                } else {
+                    this.resetPlayer();
+                }
+            break;
+            case 'up':
+                var nextYmove = this.y + this.MOVEY;
+                if(nextYmove >= -35) {
+                    this.y = this.y - this.MOVEY;
+                } else {
+                    this.resetPlayer();
+                }
+            break;
+            case 'down':
+                var nextYmove = this.y - this.MOVEY;
+                if(nextYmove <= 360) {
+                    this.y = this.y + this.MOVEY;
+                } else {
+                    this.resetPlayer();
+                }
+            break;
+        }
 
-    // console.log('player: ' + this.x + ' ' + this.y);
+    // console.log(this.isValidMove());
+
+    console.log('player: ' + this.x + ' ' + this.y);
 }
 
-
+Player.prototype.resetPlayer = function() {
+    this.x = 200;
+    this.y = 380;
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
